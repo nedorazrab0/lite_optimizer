f4fc351f3e4e28c00760338f8ae9ed8eb19afd36
nedorazrab0 <141725500+nedorazrab0@users.noreply.github.com>
2025-02-04 18:47:21 +0300

    fc

---
+++ service.sh
@@ -0,0 +1,71 @@
+MODDIR="${0%/*}"
+
+best_tcc() {
+  for tcc in "${@}"; do
+    sysctl 'net.ipv4.tcp_available_congestion_control' | grep -q "${tcc}" \
+      && sysctl -qw "net.ipv4.tcp_congestion_control=${tcc}" && break
+  done
+}
+
+best_dqd() {
+  for dqd in "${@}"; do
+    sysctl -qw "net.core.default_qdisc=${dqd}"
+  done
+}
+
+# Sleep mode
+echo 'deep' > /sys/power/mem_sleep
+
+# Zram
+szblk='/sys/block/zram0'
+if [[ -d "${szblk}" ]]; then
+  swapoff /dev/zram0
+  echo '1' > "${szblk}/reset"
+
+  ## Best compression alg
+  echo 'lz4' > "${szblk}/comp_algorithm"
+  echo 'lz4hc' > "${szblk}/comp_algorithm"
+  echo 'lzo-rle' > "${szblk}/comp_algorithm"
+  echo 'lzo' > "${szblk}/comp_algorithm"
+  echo 'zstd' > "${szblk}/comp_algorithm"  # Best
+
+  ## Zram size
+  sysmem="$(free -b | grep '^Mem:' | cut -d ' ' -f6)"
+  halfmem="$((sysmem/2))"
+  zramsize="$((sysmem + halfmem))"  # i.e. "*1.5"
+  echo "${zramsize}" > "${szblk}/disksize"
+  mkswap /dev/zram0
+  swapon -d -p32767 /dev/zram0
+fi
+
+# blk tune
+for sblk in /sys/block/*/queue; do
+  echo 'noop' > "${sblk}/scheduler"
+  echo 'none' > "${sblk}/scheduler"  # Best
+  echo '512' > "${sblk}/read_ahead_kb"
+  echo '128' > "${sblk}/nr_requests"
+  echo '2' > "${sblk}/rq_affinity"
+  echo '0' > "${sblk}/nomerges"
+  echo '0' > "${sblk}/rotational"
+done
+
+# fstrim
+mount -o 'remount,nodiscard' /data
+fstrim /data
+
+# NTP
+settings put global ntp_server 'pool.ntp.org'
+
+# AOT compilation
+cmd package compile -m speed -p PRIORITY_BACKGROUND -a &
+
+# Wait for unblock
+until [[ -d '/data/media/0/Android' ]]; do
+  sleep 1
+done
+
+# sysctl
+sysctl -p "${MODDIR}/sysctl.conf"
+
+best_tcc bbr cubic  # Right is the worst
+best_dqd sfq fq_codel fq cake  # Right is the best  
